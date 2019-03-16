2b9585187fefac3f2d837c527352494bc1e49dc5
Lucian Cumpata <cumpata.lu@outlook.com>
2019-03-16 15:08:23 +0200

    Unit test skeleton

--- CarService/Core.cs
+++ CarService/Core.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace CarService
 {
-    public class Class1
+    public class Core
     {
+        public static void TestClient()
+        {
+            //Client x = new Client();
+            using (ModelCarServiceContainer context = new ModelCarServiceContainer())
+            {
+                Client client = new Client()
+                {
+                    Nume = "Cumpata",
+                    Prenume = "Lucian",
+                    Telefon = "0720345686",
+                    Email = "cumpata.lu@outlook.com"
+                };
+
+                Auto auto = new Auto()
+                {
+                    Client = client,
+                    NumarAuto = "IS 666 LOL"
+                };
+
+                context.ClientSet.Add(client);
+                context.AutoSet.Add(auto);
+                context.SaveChanges();
+            }
+        }
+
     }
 }
---
+++ UnitTestCarService/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCarService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCarService")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("479686d2-f1d0-4107-bd26-a61a6abeb8a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

41f62b606b126b2f2c2e4425ddc6b946988589b6
Lucian Cumpata <cumpata.lu@outlook.com>
2019-05-30 10:25:00 +0300

    Updated MainWindow

---
+++ WpfCarService/OrdersDetails.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfCarService
+{
+    /// <summary>
+    /// Interaction logic for OrdersDetails.xaml
+    /// </summary>
+    public partial class OrdersDetails : Window
+    {
+        public OrdersDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
